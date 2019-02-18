bca60575063bd23dc465a6455b16f4a2d74b6578
sofia <sofia@virtualsw.com>
2019-02-18 15:56:52 +0100

    Inherit from AbstractPersistenceEventListener

--- grails-app/init/mtschema/BootStrap.groovy
+++ grails-app/init/mtschema/BootStrap.groovy
@@ -1,8 +1,22 @@
 package mtschema
 
+import grails.gorm.multitenancy.Tenants
+import org.grails.orm.hibernate.HibernateDatastore
+
 class BootStrap {
 
+    def grailsApplication
+    HibernateDatastore hibernateDatastore
+
     def init = { servletContext ->
+        println " -- En bootstrap"
+        def ctx = grailsApplication.mainContext
+        ['TEST', 'TRES'].each { String name ->
+            println " ---- Registrando ${name}"
+            HibernateDatastore ds = hibernateDatastore.getDatastoreForConnection(name)
+            VehicleListenerService listener = new VehicleListenerService(ds)
+            ctx.addApplicationListener(listener)
+        }
     }
     def destroy = {
     }
--- grails-app/services/mtschema/VehicleListenerService.groovy
+++ grails-app/services/mtschema/VehicleListenerService.groovy
@@ -2,22 +2,45 @@ package mtschema
 
 import grails.events.annotation.gorm.Listener
 import groovy.transform.CompileStatic
+import org.grails.datastore.mapping.core.Datastore
+import org.grails.datastore.mapping.engine.event.AbstractPersistenceEvent
+import org.grails.datastore.mapping.engine.event.AbstractPersistenceEventListener
 import org.grails.datastore.mapping.engine.event.PreInsertEvent
 import org.grails.datastore.mapping.engine.event.PreUpdateEvent
+import org.springframework.context.ApplicationEvent
 
 @CompileStatic
-class VehicleListenerService {
+class VehicleListenerService extends AbstractPersistenceEventListener {
+
+    protected VehicleListenerService(Datastore datastore) {
+        super(datastore)
+        println " --- Constructor. Datastore parámetro: ${datastore}. Datastore final: ${this.datastore}"
+    }
 
     @Listener(Vehicle)
     void onPreInsertEvent(PreInsertEvent event) {
-        println "*** Vehicle preInsert"
+        println "*** Vehicle preInsert: ${event.entityAccess.getProperty('model')}. Source: ${event.source}. Datastore: ${this.datastore}. Evento ${event}"
         event.entityAccess.setProperty('model', 'preInsert')
     }
 
     @Listener(Vehicle)
     void onPreUpdateEvent(PreUpdateEvent event) {
-        println "*** Vehicle preUpdate"
+        println "*** Vehicle preUpdate: ${event.entityAccess.getProperty('model')}. Source: ${event.source}. Datastore: ${this.datastore}. Evento ${event}"
         event.entityAccess.setProperty('model', 'preUpdate')
     }
+
+    @Override
+    protected void onPersistenceEvent(AbstractPersistenceEvent event) {
+        println ">>> onPersistenceEvent: ${event.entityAccess.getProperty('model')}. Source: ${event.source}. Datastore: ${this.datastore}. Evento ${event}"
+        event.entityAccess.setProperty('model', 'onPersistenceEvent')
+    }
+
+    @Override
+    boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        boolean supportsEvent = eventType.isAssignableFrom(PreInsertEvent) ||
+                eventType.isAssignableFrom(PreUpdateEvent)
+        println "??? SupportsEventType ${eventType}: ${supportsEvent}"
+        return supportsEvent
+    } // Copiado de http://www.tothenew.com/blog/using-hibernate-events-with-persistenceeventlistener/
 
 }
